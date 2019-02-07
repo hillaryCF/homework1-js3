149ebef146fa9cf212e5f7f3aba6ca6fbe5db1c4
Hilary Cordero Flores <h.cordero@WEB-07.local>
2019-02-07 17:12:15 -0600

    cambios al js y html

--- tarea1-javascript/src/js/script.js
+++ tarea1-javascript/src/js/script.js
@@ -11,17 +11,20 @@ resetButton.addEventListener('click', reset);
 save_button.addEventListener('click', save);
 
 function reset() {
+  name.value = ' ';
+  amount.value = ' ';
+  date1.value = ' ';
+  option.value = ' ';
 }
 function save() {
   nuevos();
-  resultado()
 }
 
 function nuevos() {
-  const table = document.getElementById('table');
+
   const tr = document.createElement('tr');
   const values = [name.value, option.value, date1.value, amount.value];
-  const array = [values];
+  const array = [{name: name.value},{amount: amount.value},{date: date1.value},{option: option.value}];
   console.log(array);
 
   for (let i = 0; i < 4; i += 1) {
@@ -31,7 +34,23 @@ function nuevos() {
     tr.appendChild(td);
   }
   container.appendChild(tr);
+
+  if (option.value == 'A') {
+    let arreglo = amount.value;
+    let suma = 0;
+    for (let a = 0; a < arreglo.length; a ++ ) {
+      suma += arreglo[a];
+      console.log(suma);
+    }
+  }else if (option.value == 'B') {
+
+  }
 }
-function resultado () {
-  for ()
-}
+
+// function answer(suma) {
+//   const list = document.getElementById('list');
+//   const listA = document.createElement('ul');
+//   const listAText = document.createTextNode('Bills A ' + suma);
+//   listA.appendChild(listAText);
+//   list.appendChild(listA);
+// }
